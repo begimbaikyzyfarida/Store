d4ecb2c10ca6a04942c35464370674184272c7d8
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-14 17:56:48 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+import product.*;
+
+import static product.Store.randomProduct;
+public class Main {
+    public static void main(String[] args) {
+
+        Product[] products = new Product[]{
+                new Milk("Milk",60),
+                new Milk("Milk",60),
+                new Milk("Milk",60),
+                new Milk("Milk",60),
+                new Milk("Milk",60),
+                new Corn("Corn",150),
+                new Corn("Corn",150),
+                new Corn("Corn",150),
+                new Corn("Corn",150),
+                new Corn("Corn",150),
+                new Salt("Salt",0),
+                new Salt("Salt",0),
+                new Salt("Salt",0),
+                new Salt("Salt",0),
+                new Salt("Salt",0),
+                new Stew("Stew",180),
+                new Stew("Stew",180),
+                new Stew("Stew",180),
+                new Stew("Stew",180),
+                new Stew("Stew",180),
+                new Fish("Fish",20),
+                new Fish("Fish",20),
+                new Fish("Fish",20),
+                new Fish("Fish",20),
+                new Fish("Fish",20),
+        };
+        randomProduct(products);
+        Store store = new Store();
+        store.setProducts(products);
+        store.doInspection();
+
+    }
+}
